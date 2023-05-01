3b0d97a67b4f1bdfebf9661acb39e4085100b3a2
Sira <siraphop_2538@hotmail.com>
2023-05-01 22:25:50 +1000

    Simple X/O gameplay
    - Player can now play simple X/O but win condition hasn't been implemented yet

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const gridModule = (() => {
   const allGrid = document.querySelectorAll(".grid-item");
+  let player1Turn = true;
+  let player2Turn = false;
 
   const _clickValue = function () {
-    allGrid.forEach((value) => {
-      value.addEventListener("click", () => {
-        value.textContent = "X";
+    allGrid.forEach((grid) => {
+      grid.addEventListener("click", () => {
+        if (grid.textContent === "") {
+          if (player1Turn === true) {
+            grid.textContent = "X";
+            player1Turn = false;
+            player2Turn = true;
+          } else if (player2Turn === true) {
+            grid.textContent = "O";
+            player1Turn = true;
+            player2Turn = false;
+          }
+        } else if (grid.textContent === "X" || grid.textContent === "O") {
+          console.log("spot taken mate");
+        }
       });
     });
   };
@@ -22,6 +36,8 @@ const optionModule = (() => {
   const _restart = () => {
     restart.addEventListener("click", () => {
       gridModule.allGrid.forEach((value) => (value.textContent = ""));
+      player1Turn = true;
+      player2Turn = false;
     });
   };
 
